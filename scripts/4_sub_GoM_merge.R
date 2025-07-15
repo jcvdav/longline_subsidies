7c2b8c5e0e7d6cc74e667c70e8c1b58c07f7852d
Juan Carlos Villaseñor-Derbez <jc_villasenor@miami.edu>
2025-07-15 15:10:11 -0400

    Renamed merging script and added commented instructions for Aubri.

--- scripts/4_sub_GoM_merge.R
+++ scripts/4_sub_GoM_merge.R
@@ -21,13 +21,17 @@ pacman::p_load(
 # Load data --------------------------------------------------------------------
 effort <- readRDS(file = here("data", "processed", "annual_effort_by_vessel.rds"))
 landings <- readRDS(file = here("data", "processed", "annual_landings_by_vessel.rds"))
+subsidies <- readRDS(file = here("data", "processed", "annual_subsidies_by_economic_unit.rds")) # I added this on July 15, 2025
 
 ## PROCESSING ##################################################################
 
 # X ----------------------------------------------------------------------------
 data <- inner_join(effort, landings, by = join_by(year, vessel_rnpa)) |> 
+  # Aubri, add a left join here
   mutate(period = ifelse(year <= 2019, "subsidies", "no subsidies")) |> 
-  select(period, year, vessel_id = vessel_rnpa, effort_hours = h, catch_kg = live_weight) |> 
+  # Aubri, look at the documentation for replace_na (use ?replace_na in the console, yes, with the question mark first)
+  # You will need to replace NAs in the "treated" and "subsidy_pesos" columns with 0.
+  select(period, year, eu_id = eu_rnpa, vessel_id = vessel_rnpa, effort_hours = h, catch_kg = live_weight) |> # Select the appropriate columns here
   mutate(cpue = catch_kg / effort_hours)
 
 ## VISUALIZE ###################################################################
