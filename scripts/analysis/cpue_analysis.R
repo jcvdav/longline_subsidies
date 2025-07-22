d1d4e539778be6dce81570091033f94ac5ed7320
aubrilongnecker <abl141@miami.edu>
2025-07-22 16:45:53 -0400

    reorganized scripts into separate folders for data analysis & data processing. added subsidy counts to cpue_analysis

--- scripts/analysis/cpue_analysis.R
+++ scripts/analysis/cpue_analysis.R
@@ -1,3 +1,6 @@
+#load packages
+library(dplyr)
+
 #load original data
 cpue_by_vessel <- read.csv("data/estimation/annual_effort_and_catch_by_vessel.csv")
 
@@ -15,16 +18,26 @@ cpue_clean <- cpue_by_vessel[cpue_by_vessel$cpue >= lower_bound & cpue_by_vessel
 #identifying outliers
 cpue_outliers <- cpue_by_vessel[cpue_by_vessel$cpue <= lower_bound | cpue_by_vessel$cpue >= upper_bound, ]
 
-#two sample t test for cpue, effort, and catch, before and after subsidy removal
+#significance tests-------------------------------------------------
 
-#significance test for difference in cpue before and after subsidies
+#cpue before and after subsidies
 t.test(cpue ~ period, data = cpue_clean)
 #16.58% increase in cpue, p = 0.005034
 
-#significance test for difference in effort
+#effort before and after subsidies
 t.test(effort_hours ~ period, data = cpue_clean)
 #14.03% decrease in effort, p = 0.00315
 
-#significance test for difference in catch
+#catch before and after subsidies
 t.test(catch_kg ~ period, data = cpue_clean)
 #3.68% decrease in catch, p = 0.5637 NO SIGNIFICANCE
+
+#subsidy counts-----------------------------------------------------
+
+subsidy_counts <- cpue_clean |> 
+  filter(period == "subsidies") |> 
+  group_by(vessel_id) |> 
+  summarise(years_subsidized = n_distinct(year))
+
+count(subsidy_counts, years_subsidized)
+
