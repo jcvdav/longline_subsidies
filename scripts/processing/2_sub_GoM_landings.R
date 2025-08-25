94ba7c51bffd9b956c6e265a6342128de8dc9266
jcvdav <juancarlos.villader@gmail.com>
2025-08-25 08:44:02 -0400

    Updating landings data from updated mex-fisheries version

--- scripts/processing/2_sub_GoM_landings.R
+++ scripts/processing/2_sub_GoM_landings.R
@@ -20,7 +20,7 @@ pacman::p_load(
 
 # Load data --------------------------------------------------------------------
 effort <- readRDS(file = here("data", "processed", "annual_effort_by_vessel.rds"))
-landings <- readRDS(file = here("../mex_fisheries/data/mex_landings/clean/mex_annual_landings_by_vessel.rds")) # Will only work on JC's computers for now
+landings <- readRDS(file = url("https://github.com/jcvdav/mex_fisheries/raw/refs/heads/main/data/mex_landings/clean/mex_annual_landings_by_vessel.rds"))
 
 ## PROCESSING ##################################################################
 
